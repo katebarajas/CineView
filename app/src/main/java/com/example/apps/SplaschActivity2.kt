5926233ba98f3467b866a1f45db198afe168d14c
Camilo Castañeda <camilol1021k@gmail.com>
2022-10-28 14:54:26 -0500

    splasch y cambios en el todo, about

---
+++ app/src/main/java/com/example/apps/SplaschActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.apps
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplaschActivity2 : AppCompatActivity() {
+    private val splashScreentimeout:Long=2500
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splasch2)
+        Handler().postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        },splashScreentimeout)
+    }
+}
