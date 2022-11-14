2fab948b0066a09a656d7cd0c9a74e197c3474ac
stevenbarrios2022 <stevenbarrios113@gmail.com>
2022-11-14 08:13:03 -0500

    Por fin Boton de Eliminar

--- app/src/main/java/com/example/apps/DetailFragment.kt
+++ app/src/main/java/com/example/apps/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.example.apps
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,7 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.example.apps.room_database.ToDo
+import com.example.apps.room_database.ToDoDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class DetailFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +50,34 @@ class DetailFragment: Fragment() {
             principal.putExtra("hora",tvHora.text as String)
             principal.putExtra("lugar",tvLugar.text as String)
             principal.putExtra("id",tvID.text as String)
+            startActivity(principal)
+        }
+
+        val btnBorrar : Button= fragmento.findViewById(R.id.btnDelete)
+        btnBorrar.setOnClickListener {
+            val db = ToDoDatabase.getDatabase(requireActivity())
+            val  toDoDAO= db.todoDao()
+            val dbFirebase= FirebaseFirestore.getInstance()
+            val task = ToDo(id!!.toInt(), tarea!!.toString(),hora!!.toString(),lugar!!.toString())
+
+            val negativeButton = { _: DialogInterface, _: Int -> }
+            val positiveButton = { dialog: DialogInterface, which: Int -> }
+                val builder = AlertDialog.Builder(requireContext())
+                builder.setTitle("Delete Task")
+                builder.setMessage("Realmente quieres borrar esta tarea?")
+                builder.setPositiveButton("ok", positiveButton)
+                builder.setNegativeButton("cancelar",negativeButton)
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+
+            runBlocking {
+                launch {
+                    toDoDAO.deleteTask(task)
+                    dbFirebase.collection("ToDo").document(id).delete()
+
+                }
+            }
+            val principal=Intent(requireContext(), ToDoActivity::class.java)
             startActivity(principal)
         }
 
